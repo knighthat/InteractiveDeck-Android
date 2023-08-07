d48fb43c505fb2b9c58c685b510ce4fadd12e8a7
KnightHat <knightchanelgaming@gmail.com>
2023-08-06 22:02:23 -0500

    changed name

--- app/src/main/java/me/knighthat/interactivedeck/component/Buttons.java
+++ app/src/main/java/me/knighthat/interactivedeck/component/Buttons.java
@@ -1,31 +1,45 @@
 package me.knighthat.interactivedeck.component;
 
-import com.google.gson.JsonElement;
-
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-import me.knighthat.interactivedeck.activity.DefaultActivity;
 import me.knighthat.interactivedeck.component.ibutton.IButton;
 
 public class Buttons {
 
-    private static final @NotNull List<IButton> buttons = new ArrayList<>();
+    private static final @NotNull Map<UUID, IButton> BUTTONS = new HashMap<>();
 
-    public static @NotNull List<IButton> buttons() {
-        return Collections.unmodifiableList(buttons);
+    public static @NotNull Collection<IButton> list() {
+        return Collections.unmodifiableCollection(BUTTONS.values());
     }
 
-    public static void add( @NotNull String id, @NotNull JsonElement element ) {
-        IButton btn = new IButton(DefaultActivity.INSTANCE, id);
-        btn.update(element.getAsJsonObject());
-        buttons.add(btn);
+    public static void add( @NotNull IButton button ) {
+        BUTTONS.put(button.uuid(), button);
     }
 
     public static void clear() {
-        buttons.clear();
+        BUTTONS.clear();
+    }
+
+    public static @Nullable IButton get( @NotNull UUID uuid ) {
+        return BUTTONS.getOrDefault(uuid, null);
+    }
+
+    public static @Nullable IButton get( @NotNull String id ) {
+        return get(UUID.fromString(id));
+    }
+
+    public static void remove( @NotNull UUID uuid ) {
+        BUTTONS.remove(uuid);
+    }
+
+    public static void remove( @NotNull String id ) {
+        remove(UUID.fromString(id));
     }
 }
