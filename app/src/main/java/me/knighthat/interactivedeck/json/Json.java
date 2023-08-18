2a13d5a8949bcc767e6c1f271b80ca9e5c3b5199
KnightHat <knightchanelgaming@gmail.com>
2023-08-18 12:36:38 -0500

    new parsing method

--- app/src/main/java/me/knighthat/interactivedeck/json/Json.java
+++ app/src/main/java/me/knighthat/interactivedeck/json/Json.java
@@ -1,8 +1,12 @@
 package me.knighthat.interactivedeck.json;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -10,12 +14,29 @@ import org.jetbrains.annotations.Nullable;
 
 public class Json {
 
-    @Contract ( "_ -> null" )
-    public static @Nullable JsonObject validate( @NotNull String str ) {
+    @Contract("_ -> null")
+    public static @Nullable JsonObject validate(@NotNull String str) {
         try {
             return JsonParser.parseString(str).getAsJsonObject();
         } catch (JsonParseException e) {
             return null;
         }
+    }
+
+    public static @NotNull JsonElement parse(@Nullable Object obj) {
+        JsonElement element;
+        if (obj == null) {
+            element = JsonNull.INSTANCE;
+        } else if (obj instanceof Number) {
+            element = new JsonPrimitive((Number) obj);
+        } else if (obj instanceof Boolean) {
+            element = new JsonPrimitive((Boolean) obj);
+        } else if (obj instanceof Iterable<?>) {
+            element = new JsonArray();
+            ((Iterable<?>) obj).forEach(e -> ((JsonArray) element).add(parse(e)));
+        } else {
+            element = new JsonPrimitive(String.valueOf(obj));
+        }
+        return element;
     }
 }
