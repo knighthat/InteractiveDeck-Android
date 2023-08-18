dc3fd721119c1b85d2f54a3ce48f230959e892d0
KnightHat <knightchanelgaming@gmail.com>
2023-08-18 12:37:33 -0500

    added method to save json file

--- app/src/main/java/me/knighthat/interactivedeck/json/Json.java
+++ app/src/main/java/me/knighthat/interactivedeck/json/Json.java
@@ -1,5 +1,7 @@
 package me.knighthat.interactivedeck.json;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
@@ -12,31 +14,47 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Json {
 
-    @Contract("_ -> null")
-    public static @Nullable JsonObject validate(@NotNull String str) {
+    private static final @NotNull Gson GSON;
+
+    static {
+        GSON = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    @Contract ( "_ -> null" )
+    public static @Nullable JsonObject validate( @NotNull String str ) {
         try {
-            return JsonParser.parseString(str).getAsJsonObject();
-        } catch (JsonParseException e) {
+            return JsonParser.parseString( str ).getAsJsonObject();
+        } catch ( JsonParseException e ) {
             return null;
         }
     }
 
-    public static @NotNull JsonElement parse(@Nullable Object obj) {
+    public static @NotNull JsonElement parse( @Nullable Object obj ) {
         JsonElement element;
-        if (obj == null) {
+        if ( obj == null ) {
             element = JsonNull.INSTANCE;
-        } else if (obj instanceof Number) {
-            element = new JsonPrimitive((Number) obj);
-        } else if (obj instanceof Boolean) {
-            element = new JsonPrimitive((Boolean) obj);
-        } else if (obj instanceof Iterable<?>) {
+        } else if ( obj instanceof Number ) {
+            element = new JsonPrimitive( (Number) obj );
+        } else if ( obj instanceof Boolean ) {
+            element = new JsonPrimitive( (Boolean) obj );
+        } else if ( obj instanceof Iterable<?> ) {
             element = new JsonArray();
-            ((Iterable<?>) obj).forEach(e -> ((JsonArray) element).add(parse(e)));
+            ( (Iterable<?>) obj ).forEach( e -> ( (JsonArray) element ).add( parse( e ) ) );
         } else {
-            element = new JsonPrimitive(String.valueOf(obj));
+            element = new JsonPrimitive( String.valueOf( obj ) );
         }
         return element;
+    }
+
+    public static void save( @NotNull JsonObject json, @NotNull File file ) throws IOException {
+        FileWriter writer = new FileWriter( file );
+        GSON.toJson( json, writer );
+        writer.close();
     }
 }
