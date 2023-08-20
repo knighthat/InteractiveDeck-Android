6caa755a8fd4ffd8280da68f8d05aa9a2f8c577c
KnightHat <knightchanelgaming@gmail.com>
2023-08-19 22:36:14 -0500

    implemented new json parsing

--- app/src/main/java/me/knighthat/interactivedeck/connection/DeviceInfo.java
+++ app/src/main/java/me/knighthat/interactivedeck/connection/DeviceInfo.java
@@ -3,26 +3,20 @@ package me.knighthat.interactivedeck.connection;
 import android.os.Build;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 
 import org.jetbrains.annotations.NotNull;
+
+import me.knighthat.interactivedeck.json.Json;
 
 public class DeviceInfo {
 
     public static @NotNull JsonObject json() {
-        JsonPrimitive brand = new JsonPrimitive(Build.BRAND);
-        JsonPrimitive device = new JsonPrimitive(Build.DEVICE);
-        JsonPrimitive manufacturer = new JsonPrimitive(Build.MANUFACTURER);
-        JsonPrimitive model = new JsonPrimitive(Build.MODEL);
-        int androidVer = Integer.parseInt(Build.VERSION.RELEASE);
-        JsonPrimitive androidVersion = new JsonPrimitive(androidVer);
-
         JsonObject json = new JsonObject();
-        json.add("brand", brand);
-        json.add("device", device);
-        json.add("manufacturer", manufacturer);
-        json.add("model", model);
-        json.add("androidVersion", androidVersion);
+        json.add("brand", Json.parse(Build.BRAND));
+        json.add("device", Json.parse(Build.DEVICE));
+        json.add("manufacturer", Json.parse(Build.MANUFACTURER));
+        json.add("model", Json.parse(Build.MODEL));
+        json.add("androidVersion", Json.parse(Build.VERSION.RELEASE));
 
         return json;
     }
