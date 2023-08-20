e1ecc30d003dea9f22d2b116641e4f4a12ddecba
KnightHat <knightchanelgaming@gmail.com>
2023-08-19 22:35:52 -0500

    handled add request/reworked pair request

--- app/src/main/java/me/knighthat/interactivedeck/connection/request/RequestHandler.java
+++ app/src/main/java/me/knighthat/interactivedeck/connection/request/RequestHandler.java
@@ -6,33 +6,47 @@ import com.google.gson.JsonObject;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import me.knighthat.interactivedeck.activity.ButtonsLayout;
 import me.knighthat.interactivedeck.activity.DefaultActivity;
-import me.knighthat.interactivedeck.component.Buttons;
-import me.knighthat.interactivedeck.component.ibutton.IButton;
+import me.knighthat.interactivedeck.connection.wireless.WirelessSender;
+import me.knighthat.interactivedeck.console.Log;
+import me.knighthat.interactivedeck.file.Profile;
+import me.knighthat.interactivedeck.profile.Profiles;
 
 public class RequestHandler {
 
     public static void process( @NotNull Request request ) {
         JsonElement content = request.getContent();
 
-        switch (request.getType()) {
+        switch ( request.getType() ) {
             case ADD:
-                handleAdd(content);
+                handleAdd( content );
                 break;
             case REMOVE:
-                handleRemove(content);
+                handleRemove( content );
                 break;
             case UPDATE:
-                handleUpdate(content);
+                handleUpdate( content );
                 break;
             case PAIR:
-                handlePairing(content);
+                handlePairing( content );
                 break;
         }
     }
 
     static void handleAdd( @NotNull JsonElement content ) {
-
+        JsonArray array = content.getAsJsonArray();
+        array.forEach( pJson -> {
+            JsonObject json = pJson.getAsJsonObject();
+            Profile profile = Profile.Companion.fromJson( json );
+            Profiles.PROFILES.add( profile );
+            if ( profile.isDefault() )
+                DefaultActivity.HANDLER.post( () -> ButtonsLayout.Companion.getButtons().set( profile.buttons() ) );
+        } );
     }
 
     static void handleRemove( @NotNull JsonElement content ) {
@@ -42,23 +56,32 @@ public class RequestHandler {
     static void handleUpdate( @NotNull JsonElement content ) {
         JsonObject json = content.getAsJsonObject();
 
-        String idStr = json.get("uuid").getAsString();
-        IButton button = Buttons.get(idStr);
+        String pIdStr = json.get( "profile" ).getAsString();
+        UUID pId = UUID.fromString( pIdStr );
 
-        DefaultActivity.HANDLER.post(() -> {
-            if (button != null)
-                button.update(json);
-        });
+        String btnIdStr = json.get( "button_id" ).getAsString();
+        UUID btnId = UUID.fromString( btnIdStr );
+
+        Profile profile = Profiles.get( pId );
+        JsonObject button = json.get( "button" ).getAsJsonObject();
+        if ( profile != null )
+            DefaultActivity.HANDLER.post( () -> profile.buttons().forEach( btn -> {
+                if ( btn.getUuid().equals( btnId ) )
+                    btn.update( button );
+
+                Log.deb( "Button updated" );
+            } ) );
+        Log.deb( "Handle completed!" );
     }
 
     static void handlePairing( @NotNull JsonElement content ) {
-        JsonArray btnArray = content.getAsJsonArray();
+        JsonArray json = content.getAsJsonArray();
+        List<String> ids = new ArrayList<>();
 
-        btnArray.forEach(btn -> {
-            JsonObject btnJson = btn.getAsJsonObject();
-            IButton button = new IButton(DefaultActivity.INSTANCE, btnJson);
-            Buttons.add(button);
-        });
+        json.forEach( id -> ids.add( id.getAsString() ) );
+
+        Request request = new AddRequest( ids );
+        WirelessSender.send( request );
 
         DefaultActivity.INSTANCE.startBtnLayout();
     }
