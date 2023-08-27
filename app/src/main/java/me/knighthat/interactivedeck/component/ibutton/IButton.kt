64266ed1338cc15fc9704d9aab1e1cc178ac25f0
KnightHat <knightchanelgaming@gmail.com>
2023-08-27 11:48:32 -0500

    moved to data class

--- app/src/main/java/me/knighthat/interactivedeck/component/ibutton/IButton.kt
+++ app/src/main/java/me/knighthat/interactivedeck/component/ibutton/IButton.kt
@@ -4,45 +4,47 @@ import android.widget.GridLayout
 import androidx.appcompat.widget.AppCompatButton
 import com.google.gson.JsonObject
 import com.google.gson.JsonSyntaxException
-import me.knighthat.interactivedeck.activity.DefaultActivity
+import me.knighthat.interactivedeck.event.EventHandler
 import me.knighthat.interactivedeck.task.GotoPage
 import me.knighthat.interactivedeck.task.Task
 import me.knighthat.interactivedeck.utils.ColorUtils
 import java.util.UUID
 
 
-class IButton(json: JsonObject) : AppCompatButton(DefaultActivity.INSTANCE) {
+data class IButton(
+    val uuid: UUID,
+    val x: Int,
+    val y: Int,
+    var task: Task?
+) : AppCompatButton(EventHandler.DEF_ACTIVITY) {
 
-    val uuid: UUID
-    val x: Int
-    val y: Int
-    var task: Task? = null
+    companion object {
+        fun fromJson(json: JsonObject): IButton {
+            if (!json.has("uuid") ||
+                !json.has("background") ||
+                !json.has("foreground") ||
+                !json.has("text") ||
+                !json.has("x") ||
+                !json.has("y")
+            )
+                throw JsonSyntaxException("Not enough argument")
 
-    init {
-        if (!json.has("uuid") ||
-            !json.has("background") ||
-            !json.has("foreground") ||
-            !json.has("text") ||
-            !json.has("x") ||
-            !json.has("y")
-        )
-            throw JsonSyntaxException("Not enough argument")
+            val uuidStr = json["uuid"].asString
+            val uuid = UUID.fromString(uuidStr)
+            val x = json.getAsJsonPrimitive("x").asInt
+            val y = json.getAsJsonPrimitive("y").asInt
 
-        val uuid = json["uuid"].asString
-        this.uuid = UUID.fromString(uuid)
+            val button = IButton(uuid, x, y, null)
 
-        val x = json.getAsJsonPrimitive("x")
-        this.x = x.asInt
+            val params = GridLayout.LayoutParams()
+            params.rowSpec = GridLayout.spec(y, 1, 1f)
+            params.columnSpec = GridLayout.spec(x, 1, 1f)
+            button.layoutParams = params
 
-        val y = json.getAsJsonPrimitive("y")
-        this.y = y.asInt
+            button.update(json)
 
-        val params = GridLayout.LayoutParams()
-        params.rowSpec = GridLayout.spec(this.y, 1, 1f)
-        params.columnSpec = GridLayout.spec(this.x, 1, 1f)
-        this.layoutParams = params
-
-        this.update(json)
+            return button
+        }
     }
 
     fun update(json: JsonObject) {
