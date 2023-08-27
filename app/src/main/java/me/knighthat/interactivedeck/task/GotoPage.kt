83f2260ba64660177b0b0ec2429d99f61b811ff1
KnightHat <knightchanelgaming@gmail.com>
2023-08-27 12:44:03 -0500

    moved to kotlin data class

--- app/src/main/java/me/knighthat/interactivedeck/task/GotoPage.kt
+++ app/src/main/java/me/knighthat/interactivedeck/task/GotoPage.kt
@@ -2,11 +2,4 @@ package me.knighthat.interactivedeck.task
 
 import java.util.UUID
 
-class GotoPage(uuid: UUID) : Task {
-
-    val uuid: UUID
-
-    init {
-        this.uuid = uuid
-    }
-}
+data class GotoPage(val uuid: UUID) : Task
