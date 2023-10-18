a578c3d62aebf8f44d0397cc3007c85b43d1a906
KnightHat <knightchanelgaming@gmail.com>
2023-10-18 16:50:30 -0500

    moved to classes from lib

--- app/src/main/java/me/knighthat/interactivedeck/component/action/PressAction.kt
+++
@@ -1,5 +0,0 @@
-package me.knighthat.interactivedeck.component.action
-
-import me.knighthat.interactivedeck.component.ibutton.IButton
-
-class PressAction(button: IButton) : Action(type = ActionType.PRESS, button)
--- app/src/main/java/me/knighthat/interactivedeck/connection/request/PairRequest.kt
+++
@@ -1,5 +0,0 @@
-package me.knighthat.interactivedeck.connection.request
-
-import me.knighthat.interactivedeck.connection.DeviceInfo
-
-class PairRequest : Request(type = RequestType.PAIR, content = DeviceInfo().serialize())
