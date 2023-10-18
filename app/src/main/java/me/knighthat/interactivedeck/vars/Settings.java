65b0787651ffc21b60f10d2022332bd737f4c211
KnightHat <knightchanelgaming@gmail.com>
2023-10-18 16:49:35 -0500

    removed redundant message

--- app/src/main/java/me/knighthat/interactivedeck/vars/Settings.java
+++ app/src/main/java/me/knighthat/interactivedeck/vars/Settings.java
@@ -11,16 +11,13 @@ public class Settings {
     public static byte[] BUFFER = new byte[1024];
 
     public static void saveLastHost( String address, int port ) {
-        Log.deb( "Saving " + address + ":" + port );
-
         SharedPreferences.Editor editor = PREFERENCES.edit();
         editor.putString( "address", address );
         editor.putString( "port", String.valueOf( port ) );
         editor.apply();
 
         String msg = "Saved %s:%s for next login";
-        msg = String.format( msg, address(), port() );
-        Log.info( msg );
+        Log.info( String.format( msg, address(), port() ) );
     }
 
     public static String address() {
