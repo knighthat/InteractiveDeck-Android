4409de9d669ae311a16bf36fa1d029807fd3c7c9
KnightHat <knightchanelgaming@gmail.com>
2023-10-28 13:26:47 -0500

    parse task from JsonObject

--- app/src/main/java/me/knighthat/interactivedeck/task/Task.kt
+++ app/src/main/java/me/knighthat/interactivedeck/task/Task.kt
@@ -1,3 +1,26 @@
 package me.knighthat.interactivedeck.task
 
-interface Task
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import java.util.UUID
+
+interface Task {
+
+    companion object {
+        fun fromJson(json: JsonElement): Task? {
+            if (json.isJsonNull || json !is JsonObject) return null
+
+            var task: Task? = null
+            val type = json["action_type"].asString
+
+            if (type == "SWITCH_PROFILE") {
+
+                val uuid = UUID.fromString(json["profile"].asString)
+                task = GotoPage(uuid)
+
+            }
+
+            return task
+        }
+    }
+}
